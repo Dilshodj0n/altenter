9dddab476dd761aa2493fcbcf251f7c3fae9774a
Elbek7879 <abduraximovelbek7879@gmail.com>
2025-05-14 17:15:54 +0500

    yana ozgarishlar boldi

--- src/service/AdminService.java
+++ src/service/AdminService.java
@@ -1,11 +1,34 @@
 package service;
 
+import java.util.Scanner;
+
 public class AdminService {
+    Scanner scanner = new Scanner(System.in);
+    Scanner intScanner = new Scanner(System.in);
     public void service(){
         while (true){
             System.out.println("""
-                    
+                    0. exit
+                    1. sign up
+                    2. sign in
                     """);
+            switch (intScanner.nextInt()){
+                case 0 -> {
+                    return;
+                }
+                case 2 -> {
+                   signUP(); 
+                }
+                case 3 -> {
+                    signIn();
+                }
+            }
         }
+    }
+
+    private void signUP() {
+    }
+
+    private void signIn() {
     }
 }
