4f1b9344862b0d3e05efb6fe694d997adfba7c1a
Elbek7879 <abduraximovelbek7879@gmail.com>
2025-06-02 15:23:44 +0500

    nima gap

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import service.AdminService;
+
 public class Main {
     public static void main(String[] args) {
 
+        AdminService adminService = new AdminService();
+        adminService.service();
     }
 }
